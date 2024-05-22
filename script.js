d8fd6e69fa50e8c18e1381952ea01e2b741d804c
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-22 23:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Newcell1 and newcell2
 function insert_Row() {
-    //Write your code here
-  
-  
+let table = document.getElementById("sampleTable");
+
+let rowButton = document.querySelector("input"); 
+
+rowButton.addEventListener("click", display);
+
+function display(eventDetails){ 
+let addRow = table.insertRow(0);
+
+addRow.insertCell(0).innerHTML = "New Cell1";
+addRow.insertCell(1).innerHTML = "New Cell2";
+
+}
+ 
+   
 }
