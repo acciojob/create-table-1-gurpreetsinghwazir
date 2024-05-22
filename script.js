dd977a39e39a422b88dbee5d1155d8b5281749c1
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-22 23:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,9 @@
 function insert_Row() {
 let table = document.getElementById("sampleTable");
 
-let rowButton = document.querySelector("input"); 
 
-rowButton.addEventListener("click", display);
-
-function display(eventDetails){ 
 let addRow = table.insertRow(0);
-
 addRow.insertCell(0).innerHTML = "New Cell1";
 addRow.insertCell(1).innerHTML = "New Cell2";
 
-}
- 
-   
 }
